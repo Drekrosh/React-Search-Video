d4cc8f843477ab1d2fbc55f91cc3713855e31052
Rahnar <rea-90@mail.ru>
2016-02-15 03:16:56 +0300

    Добавлен API для работы с YouTube + Метод для поиска.

--- front-end/components/App.js
+++ front-end/components/App.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import _ from 'lodash';
 import SearchBar from './SearchBar/index';
 import VideoList from './VideoList/index';
+import YTSearch from 'youtube-api-search';
 import VideoDetail from './VideoDetail/index';
 
+const API_KEY = 'AIzaSyDmbw1I5gzelxHOjCRTMDkUJK27hcNVn1k';
 
 class App extends Component {
 
@@ -13,14 +15,24 @@ class App extends Component {
             videos: [],
             selectedVideo: null
         };
+        this._videoSearch('Путин');
     }
 
+    _videoSearch = (term) => {
+        YTSearch({ key: API_KEY, term: term }, (videos) => {
+            let selectedVideo = videos[0];
+            this.setState({
+                videos,
+                selectedVideo
+            });
+        });
+    };
 
     render() {
 
         return (
             <main>
-                <SearchBar/>
+                <SearchBar onSearchTerm={ _videoSearch }/>
                 <VideoDetail/>
                 <VideoList/>
             </main>
