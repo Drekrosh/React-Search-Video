c3a23c978c91f7fba8acfeaaca291cf97cf39236
Rahnar <rea-90@mail.ru>
2016-02-15 03:40:36 +0300

    Метод debounce,вызывает функцию не чаще 300мс

--- front-end/components/App.js
+++ front-end/components/App.js
@@ -10,26 +10,33 @@ const API_KEY = 'AIzaSyDmbw1I5gzelxHOjCRTMDkUJK27hcNVn1k';
 class App extends Component {
 
     constructor(props) {
+
         super(props);
         this.state = {
             videos: [],
             selectedVideo: null
         };
         this._videoSearch('Путин');
+
     }
 
     _videoSearch = (term) => {
+
         YTSearch({ key: API_KEY, term: term }, (videos) => {
+
             let selectedVideo = videos[0];
             this.setState({
                 videos,
                 selectedVideo
             });
+
         });
+
     };
 
     render() {
 
+        const _videoSearch = _.debounce(this._videoSearch, 300);
         return (
             <main>
                 <SearchBar onSearchTerm={ _videoSearch }/>
